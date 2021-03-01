7a9f511831318388aa343d0058136e5657fe0447
Aaron Bockover <abock@microsoft.com>
2021-03-01 18:19:41 -0500

    Update scratchpad

---
+++ src/eager/test_models/scratchpad.py
@@ -0,0 +1,30 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import torch
+import torch_ort
+
+device = torch_ort.device.apollo()
+
+ones = torch.ones(2, 3).to(device)
+print(ones.cpu())
+
+twos = ones + ones
+print(twos.cpu())
+
+threes = twos + ones
+print(threes.cpu())
+
+fours = twos * twos
+print(fours.cpu())
+
+fenced_ten = torch.tensor(
+  [[-1, -1, -1],
+   [-1, 10, -1],
+   [-1, -1, -1]],
+  device = device)
+
+print(fenced_ten.numel())
+print(fenced_ten.size())
+print(fenced_ten.cpu())
+print(fenced_ten.relu().cpu())
