2d1025368c5db80784bf19e938d2cbca1f62187b
Aaron Bockover <abock@microsoft.com>
2021-03-19 12:39:24 -0400

    PyTorch: add AutogradORT keys

--- src/eager/ort_tensor.h
+++ src/eager/ort_tensor.h
@@ -13,7 +13,10 @@ class ORTTensorImpl final : public c10::TensorImpl {
  public:
   explicit ORTTensorImpl(OrtValue tensor, const at::TensorOptions& options)
     : c10::TensorImpl(
-        c10::DispatchKeySet{c10::DispatchKey::ORT},
+        c10::DispatchKeySet {
+          c10::DispatchKey::ORT,
+          c10::DispatchKey::AutogradORT
+        },
         options.dtype(),
         options.device()) {
     set_tensor(tensor);
