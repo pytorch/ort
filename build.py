0303e21f2be7011364ac0ac58e808691b4ff60ef
liqunfu <liqun_fu@hotmail.com>
2021-05-25 11:18:43 -0700

    use docker files to run CI (#28)

--- build.py
+++ build.py
@@ -6,11 +6,11 @@ import subprocess
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-def run_subprocess(args, cwd=None, capture_stdout=False, shell=False):
+def run_subprocess(args, cwd=None):
     if isinstance(args, str):
         raise ValueError("args should be a sequence of strings, not a string")
 
-    return subprocess.run(args, cwd=cwd, shell=shell)
+    return subprocess.run(args, cwd=cwd, shell=False, check=True)
 
 def run_ort_module_tests(cwd, source_dir):
     args = [sys.executable, os.path.join(source_dir, 'tests/bert_for_sequence_classification.py')]
@@ -26,10 +26,16 @@ def main():
 
     build_wheel(source_dir, source_dir)
 
+    # installing torch-ort wheel
     dist_path = os.path.join(source_dir, 'dist')
     wheel_file = os.listdir(dist_path)[0]
     run_subprocess([sys.executable, "-m", "pip", "install", "--upgrade", os.path.join(dist_path, wheel_file)], cwd)
 
+    # installing requirements-test.txt
+    requirements_path = os.path.join(source_dir, 'tests', 'requirements-test.txt')
+    run_subprocess([sys.executable, "-m", "pip", "install", "-r", requirements_path], cwd)
+
+    # testing torch-ort
     run_ort_module_tests(source_dir, source_dir)
 
 if __name__ == "__main__":
