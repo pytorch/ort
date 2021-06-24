056e32191d73ef9b9227698eab2f07d9d78d51bc
Thiago Crepaldi <thiago.crepaldi@microsoft.com>
2021-06-24 13:07:19 -0700

    Add mechanism to build ORTModule's PyTorch CPP extensions
    PyTorch CPP Extensions for ORTModule can be compiled by running:
       python -m torch_ort.configure

--- torch_ort/experimental/__init__.py
+++ torch_ort/experimental/__init__.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
 """Experimental APIs for ORTModule
 
 DO NOT use these APIs for any production code as they can be removed at any time
--- torch_ort/experimental/graph_config.py
+++ torch_ort/experimental/graph_config.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
 from enum import Enum
 from onnxruntime.capi._pybind_state import PropagateCastOpsStrategy
 from torch_ort import ORTModule
