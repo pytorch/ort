e2507218719d2966a71ad085d6554298db5f80e7
Cheng Tang <souptc@gmail.com>
2021-08-27 23:36:01 +0000

    remove ort eager code base since we migrate to onnxruntime repo

--- src/eager/ort_customops.h
+++
@@ -1,9 +0,0 @@
-#include <torch/extension.h>
-
-namespace torch_ort {
-namespace eager {
-
-void GenerateCustomOpsBindings(pybind11::module_ module);
-
-} // namespace eager
-} // namespace torch_ort
