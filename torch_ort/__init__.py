79d1150f44dad7b252dd6c45ac62664d295ec280
Baiju Meswani <bmeswani@microsoft.com>
2021-09-09 21:02:23 +0000

    Adding a __version__ attribute to torch_ort

--- torch_ort/__init__.py
+++ torch_ort/__init__.py
@@ -5,3 +5,10 @@
 
 from onnxruntime.training.ortmodule import ORTModule, DebugOptions, LogLevel
 from onnxruntime import set_seed
+
+# Add a __version__ attribute that users can use to check the version
+try:
+    from ._version import __version__
+except ModuleNotFoundError:
+    # ._version.py file not found. Not setting any __version__ attribute
+    pass
