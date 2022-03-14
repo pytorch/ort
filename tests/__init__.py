e31c8857eaac0a2907ff2580561931c8de24e4f5
Devang Patel <devpatel@az-eus-v100-32gb-5-worker-00028.eastus.cloudapp.azure.com>
2022-03-14 16:02:17 +0000

    Add missing __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+import os
+import pathlib
+import sys
+sys.path.insert(0, os.path.join(pathlib.Path(__file__).parent.absolute()))
