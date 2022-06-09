d446d3401bdeb470104b8a4f5d34b02bb71ff46b
Laxmi Ganesan <laxmi.ganesan@intel.com>
2022-06-09 10:31:07 -0700

    Adding license headers

--- torch_ort_inference/tests/bert_for_sequence_classification.py
+++ torch_ort_inference/tests/bert_for_sequence_classification.py
@@ -1,3 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
 import argparse
 import os
 import numpy as np
